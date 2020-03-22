646e7041f0d317fd613db62ed5f6d282826bdec0
eien <GSKryachko@gmail.com>
2020-03-22 20:20:08 +0500

    Add very basic check (just walks the tree and does nothing)

---
+++ contrib/amcheck/sql/check_gin.sql
@@ -0,0 +1,9 @@
+-- minimal test, basically just verifying that amcheck works with GiST
+SELECT setseed(1);
+CREATE TABLE "gin_check"("Column1" int[]);
+INSERT INTO gin_check select array_agg(round(random()*100)) from generate_series(1, 1000000) as i group by i % 100000;
+CREATE INDEX gin_check_idx on "gin_check" USING GIN("Column1");
+SELECT gin_index_parent_check('gin_check_idx');
+
+-- cleanup
+DROP TABLE gin_check;
